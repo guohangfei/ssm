c09aa1f551d6c526f830e5e394070d683faaa675
guohangfei <guohangfei@126.com>
2018-03-29 14:40:10 +0800

    webmagic  爬虫测试完善

---
+++ src/test/ghfTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+/**
+ * @Author: 郭航飞
+ * @Description: 测试类
+ * @Date: created in      2018/3/2911:24
+ */
+public class ghfTest {
+
+    @Test
+    public void donw() {
+        System.out.println(123123);
+    }
+}
